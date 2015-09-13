1d01559413b9919ee9731b5f0099b768f77e6312
Fernando Silva <fernando.debrando@gmail.com>
2015-09-12 23:40:50 -0300

    Initial Commit

---
+++ src/java/indexBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author fernando
+ */
+@ManagedBean(name = "indexBean1")
+@SessionScoped
+public class indexBean {
+
+    /**
+     * Creates a new instance of indexBean
+     */
+    public indexBean() {
+    }
+    
+}
