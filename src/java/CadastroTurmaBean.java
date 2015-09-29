10f291c26f0a0ea8815a3b9f3161e0ea1238976d
Guilherme <Guilherme@dell-gui>
2015-09-28 22:07:49 -0300

    Ajustado layout

---
+++ src/java/CadastroTurmaBean.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import Model.ConexaoMySQL;
+import Model.ExecuteSQL;
+import java.sql.SQLException;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Guilherme
+ */
+@ManagedBean
+@RequestScoped
+public class CadastroTurmaBean {
+
+    
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public void cadastroTurma() throws SQLException{
+        
+        ExecuteSQL executa = new ExecuteSQL();
+        
+        String disciplina = nome ;
+        
+        String cadastro = "INSERT INTO cad_disciplina (nome) VALUES ($disciplina)"; 
+        
+        executa.inserir(cadastro);
+        
+    }
+    public CadastroTurmaBean() {
+    }
+    
+}
